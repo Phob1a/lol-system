6eafeb135f85379ebd5b4b26d3bc88eb17bfa081
lixuan.dai <lixuan.dai@bytedance.com>
2026-06-12 11:12:41 +0800

    feat(tournament): error and template type definitions

---
+++ src/lib/tournament/errors.ts
@@ -0,0 +1,22 @@
+export type TournamentErrorCode =
+  | 'SEASON_NOT_FOUND'
+  | 'TOURNAMENT_EXISTS'
+  | 'TOURNAMENT_NOT_FOUND'
+  | 'INVALID_CONFIG'
+  | 'INVALID_STATE'
+  | 'TEAM_NOT_IN_SEASON'
+  | 'MATCH_NOT_FOUND'
+  | 'VERSION_CONFLICT'
+  | 'DOWNSTREAM_RECORDED'
+  | 'STANDINGS_TIED'
+  | 'VALIDATION';
+
+export class TournamentError extends Error {
+  constructor(
+    public code: TournamentErrorCode,
+    message: string,
+  ) {
+    super(message);
+    this.name = 'TournamentError';
+  }
+}
